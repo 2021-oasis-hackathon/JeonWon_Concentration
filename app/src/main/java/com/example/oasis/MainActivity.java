f98883a6d30faee34432a084c01fdc2a0bee0cdf
hyeonseongkang <gt9697@naver.com>
2021-08-14 21:39:37 +0900

    str2변수 추가

--- app/src/main/java/com/example/oasis/MainActivity.java
+++ app/src/main/java/com/example/oasis/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private String str = "Oasis";
+    String str2 = "Oasis2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
