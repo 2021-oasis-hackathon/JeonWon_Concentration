aa24b56a79a7913e20856d46a96b6afd0328e7d3
taewon <kimtae9217@gmail.com>
2021-08-15 17:54:47 +0900

    [ADD] 전체적인 구조 짜기

---
+++ app/src/main/java/com/example/oasis/CourseActivityAdapter.java
@@ -0,0 +1,60 @@
+package com.example.oasis;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CourseActivityAdapter extends RecyclerView.Adapter<CourseActivityAdapter.MyViewHolder> {
+
+    private List<Course> dataList;
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView place;
+        private ImageView image;
+
+        public MyViewHolder(View v) {
+            super(v);
+
+            place = (TextView) v.findViewById(R.id.place);
+            image = (ImageView) v.findViewById(R.id.image);
+        }
+    }
+
+    public CourseActivityAdapter(List<Course> list) {
+        dataList = list;
+    }
+
+    @Override
+    public CourseActivityAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_course_adapter, parent, false);
+
+        CourseActivityAdapter.MyViewHolder vh = new CourseActivityAdapter.MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(CourseActivityAdapter.MyViewHolder holder, int position) {
+
+        holder.place.setText(dataList.get(position % dataList.size()).getPlace());
+        holder.image.setImageBitmap(dataList.get(position % dataList.size()).getBitmap());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList == null ? 0 : Integer.MAX_VALUE;
+        //dataList.size()
+    }
+}
---
+++ app/src/main/java/com/example/oasis/LoadingActivity.java
@@ -0,0 +1,31 @@
+package com.example.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        loading();
+    }
+
+    private void loading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+}
