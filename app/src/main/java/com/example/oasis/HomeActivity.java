eed7cb164c3ebffaae17f928a25bda2055899fd5
hyeonseongkang <gt9697@naver.com>
2021-08-17 00:16:03 +0900

    [Feat] HomeActivity activity_home Modify

--- app/src/main/java/com/example/oasis/HomeActivity.java
+++ app/src/main/java/com/example/oasis/HomeActivity.java
@@ -13,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class HomeActivity extends Fragment {
 
     private static final String TAG = "HomeActivity";
@@ -20,9 +22,10 @@ public class HomeActivity extends Fragment {
     private View v;
 
     private ImageView jMap, jbMap;
-    private TextView textView;
+    private TextView textView, jnMap, gMap;
 
     private Button button;
+    private Button jbButton, jnButton, gButton, jbButton1, jnButton1, gButton1;
 
     public HomeActivity() {}
 
@@ -32,14 +35,113 @@ public class HomeActivity extends Fragment {
 
         v = inflater.inflate(R.layout.activity_home, container, false);
 
+        jbButton = (Button) v.findViewById(R.id.jbButton);
+        jnButton = (Button) v.findViewById(R.id.jnButton);
+        gButton = (Button) v.findViewById(R.id.gButton);
+
+
+        jbButton1 = (Button) v.findViewById(R.id.jbButton1);
+        jnButton1 = (Button) v.findViewById(R.id.jnButton1);
+        gButton1 = (Button) v.findViewById(R.id.gButton1);
+
+        jbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jnMap.setVisibility(View.GONE);
+                gMap.setVisibility(View.GONE);
+
+                jbMap.setVisibility(View.VISIBLE);
+            }
+        });
+
+        jnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gMap.setVisibility(View.GONE);
+                jbMap.setVisibility(View.GONE);
+
+                jnMap.setVisibility(View.VISIBLE);
+            }
+        });
+
+        gButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jbMap.setVisibility(View.GONE);
+                jnMap.setVisibility(View.GONE);
+
+                gMap.setVisibility(View.VISIBLE);
+            }
+        });
+
+        jbButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jbButton1.setVisibility(View.GONE);
+                jnButton1.setVisibility(View.GONE);
+                gButton1.setVisibility(View.GONE);
+
+                jbButton.setVisibility(View.VISIBLE);
+                jnButton.setVisibility(View.VISIBLE);
+                gButton.setVisibility(View.VISIBLE);
+
+                jnMap.setVisibility(View.GONE);
+                gMap.setVisibility(View.GONE);
+                jbMap.setVisibility(View.VISIBLE);
+            }
+        });
+
+        jnButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jbButton1.setVisibility(View.GONE);
+                jnButton1.setVisibility(View.GONE);
+                gButton1.setVisibility(View.GONE);
+
+                jbButton.setVisibility(View.VISIBLE);
+                jnButton.setVisibility(View.VISIBLE);
+                gButton.setVisibility(View.VISIBLE);
+
+
+                gMap.setVisibility(View.GONE);
+                jbMap.setVisibility(View.GONE);
+                jnMap.setVisibility(View.VISIBLE);
+            }
+        });
+
+        gButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jbButton1.setVisibility(View.GONE);
+                jnButton1.setVisibility(View.GONE);
+                gButton1.setVisibility(View.GONE);
+
+                jbButton.setVisibility(View.VISIBLE);
+                jnButton.setVisibility(View.VISIBLE);
+                gButton.setVisibility(View.VISIBLE);
+
+                jbMap.setVisibility(View.GONE);
+                jnMap.setVisibility(View.GONE);
+                gMap.setVisibility(View.VISIBLE);
+            }
+        });
+
         jMap = (ImageView) v.findViewById(R.id.jMap);
         jbMap = (ImageView) v.findViewById(R.id.jbMap);
+        jnMap = (TextView) v.findViewById(R.id.jnMap);
+        gMap = (TextView) v.findViewById(R.id.gMap);
 
         jMap.setEnabled(true);
         jMap.setClickable(true);
 
         jbMap.setEnabled(true);
         jbMap.setClickable(true);
+
+        jnMap.setEnabled(true);
+        jnMap.setClickable(true);
+
+        gMap.setEnabled(true);
+        gMap.setClickable(true);
 
         textView = (TextView) v.findViewById(R.id.textView);
         textView.setEnabled(true);
