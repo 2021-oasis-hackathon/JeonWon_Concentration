747a7d7d74e5801cde06dbdc63927b9c30aa8fee
hyeonseongkang <gt9697@naver.com>
2021-08-16 20:25:12 +0900

    [Feat] HomeActivity Image add

--- app/src/main/java/com/example/oasis/HomeActivity.java
+++ app/src/main/java/com/example/oasis/HomeActivity.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +19,9 @@ public class HomeActivity extends Fragment {
 
     private View v;
 
+    private ImageView jMap, jbMap;
+    private TextView textView;
+
     private Button button;
 
     public HomeActivity() {}
@@ -26,6 +31,33 @@ public class HomeActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         v = inflater.inflate(R.layout.activity_home, container, false);
+
+        jMap = (ImageView) v.findViewById(R.id.jMap);
+        jbMap = (ImageView) v.findViewById(R.id.jbMap);
+
+        jMap.setEnabled(true);
+        jMap.setClickable(true);
+
+        jbMap.setEnabled(true);
+        jbMap.setClickable(true);
+
+        textView = (TextView) v.findViewById(R.id.textView);
+        textView.setEnabled(true);
+        textView.setClickable(true);
+
+        textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                jbMap.setVisibility(View.GONE);
+            }
+        });
+
+        jMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jbMap.setVisibility(View.VISIBLE);
+            }
+        });
 
         button = (Button) v.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
