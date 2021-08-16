54c2381aaa8488192809472564b812e1e2d19662
hyeonseongkang <gt9697@naver.com>
2021-08-16 20:02:10 +0900

    [Feat] LoginActivity Firebase

--- app/src/main/java/com/example/oasis/LoginActivity.java
+++ app/src/main/java/com/example/oasis/LoginActivity.java
@@ -5,23 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRefUsers = database.getReference("Users");
+    private List<User> userList = new ArrayList<>();
+
+
     private EditText id, pw;
     private Button loginButton, member;
 
-    // database
-    List<User> userList = MainActivity.userList;
 
 
 
@@ -74,6 +85,28 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, MemberActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        userList.clear();
+        myRefUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot childrenSnapshot: dataSnapshot.getChildren()) {
+                    User userData = childrenSnapshot.getValue(User.class);
+                    userList.add(userData);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
     }
