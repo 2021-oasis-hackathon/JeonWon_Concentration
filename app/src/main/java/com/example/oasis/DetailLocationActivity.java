bbaebddb02c1528a0892002cb25a7f54bc116d0f
hyeonseongkang <gt9697@naver.com>
2021-08-17 03:37:22 +0900

    [ADD]

--- app/src/main/java/com/example/oasis/DetailLocationActivity.java
+++ app/src/main/java/com/example/oasis/DetailLocationActivity.java
@@ -8,17 +8,16 @@ import android.widget.ImageView;
 
 public class DetailLocationActivity extends AppCompatActivity {
 
-    private ImageView backButton, search;
+    private ImageView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_location);
 
-        backButton = (ImageView) findViewById(R.id.backButton);
+
         search = (ImageView) findViewById(R.id.search);
 
-        backButton.setColorFilter(Color.parseColor("#42C458"));
         search.setColorFilter(Color.parseColor("#42C458"));
     }
 }
--- app/src/main/java/com/example/oasis/LoadingActivity.java
+++ app/src/main/java/com/example/oasis/LoadingActivity.java
@@ -20,60 +20,13 @@ public class LoadingActivity extends AppCompatActivity {
 
     private static final String TAG = "LoadingActivity";
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-
-    private Button button1, button2;
-    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        editText = (EditText) findViewById(R.id.editText);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, String.valueOf(database));
-                Log.d(TAG, String.valueOf(myRef));
-                String value = editText.getText().toString();
-                myRef.setValue(value + "");
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-       // loading();
+        loading();
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-    }
-
 
     private void loading() {
         Handler handler = new Handler();
